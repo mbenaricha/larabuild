b0215ddfa8eb25df092a3540645c1f00487a5fff
bpomarelle <bpomarelle@determine.com>
2020-01-06 17:26:02 +0100

    table to display content

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,10 +6,34 @@
 
 @section('content')
     <section class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
+      <div class ="container">
+        <h1 class="display-4">Visualiser</h1>
         <p class="lead">This is a simple &laquo; information by applications &raquo;</p>
-        <p>Don't hesitate to expand this below dump.</p>
-        <hr class="my-4">
-        @php(dump($informationsByApplication))
+        {{-- <hr class="my-4"> --}}
+        {{-- @php(dump($informationsByApplication)) --}}
+        <div class="input-group col-3">
+          <input id="constantNameInput" type="text" class="form-control" placeholder="Constant Name" aria-label="constantName">
+        </div>
+          <table class="table table-dark table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Appli</th>
+                <th scope="col">Constant Name</th>
+                <th scope="col">Constant Value</th>
+              </tr>
+            </thead>
+            <tbody>
+            @foreach ($informationsByApplication as $appliName => $appli)
+              @foreach ($appli["constant"] as $constName => $constValue)
+                <tr class="{{$constName}} constantRow">
+                  <td>{{$appliName}}</td>
+                  <td>{{$constName}}</td>
+                  <td>{{$constValue}}</td>
+                </tr>
+              @endforeach
+            @endforeach
+            </tbody>
+          </table>
+      </div>
     </section>
 @endsection
