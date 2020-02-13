993ec5c537a18ba0448d5505bbf12f8147351381
Patrick FARRUGIA <pfarrugia@determine.com>
2020-02-13 16:40:37 +0100

    Fix typo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,22 @@
-require( 'datatables.net-bs4' );
+require('datatables.net-bs4');
 
 const tableElement = document.querySelector('#table');
 
-if (tableElement)  {
-   $(tableElement).DataTable();
+if (tableElement) {
+    $(tableElement).DataTable();
 
-   document.addEventListener('click', function(event) {
-      if (event.target.classList.contains('js-toggle-button')) {
-         const button = event.target;
-         const pre = button.nextElementSibling
-         if (pre.classList.contains('d-none')) {
-            pre.classList.remove('d-none');
-            button.innerText = 'Hide';
-         }
-         else {
-            pre.classList.add('d-none');
-            button.innerText = 'Display';
-         }
-      }
-   })
+    document.addEventListener('click', function (event) {
+        if (event.target.classList.contains('js-toggle-button')) {
+            const button = event.target;
+            const pre = button.nextElementSibling
+            if (pre.classList.contains('d-none')) {
+                pre.classList.remove('d-none');
+                button.innerText = 'Hide';
+            } else {
+                pre.classList.add('d-none');
+                button.innerText = 'Display';
+            }
+        }
+    })
 }
 
